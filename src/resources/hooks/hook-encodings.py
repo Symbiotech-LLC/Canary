3391265591ce2bb7f670a5767453c931351bbdfd
Grimm Venom <grimmvenom@gmail.com>
2018-11-14 21:14:24 -0500

    updated .spec

--- src/resources/hooks/hook-encodings.py
+++ src/resources/hooks/hook-encodings.py
@@ -1,12 +1,10 @@
 #-----------------------------------------------------------------------------
 # Copyright (c) 2005-2018, PyInstaller Development Team.
 #
-# Distributed under the terms of the GNU General Public License with 
-exception
+# Distributed under the terms of the GNU General Public License with exception
 # for distributing bootloader.
 #
-# The full license is in the file COPYING.txt, distributed with this 
-software.
+# The full license is in the file COPYING.txt, distributed with this software.
 #-----------------------------------------------------------------------------
 
 
